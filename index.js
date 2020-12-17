6a906f89e437152014aaf2142925d63af161d1c5
RavenVor <volodimir.vorona@gmail.com>
2020-12-18 00:07:57 +0200

    додає вирішення задачі

--- index.js
+++ index.js
@@ -7,6 +7,35 @@ const colors = [
   '#795548',
 ];
 
+let intervalId = null;
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+};
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  intervalId = setInterval(changeBodyСolor, 1000);
+
+  refs.startBtn.setAttribute('disabled', true);
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+  intervalId = null;
+
+  refs.startBtn.removeAttribute('disabled');
+}
+
+function changeBodyСolor() {
+  refs.body.style.backgroundColor =
+    colors[randomIntegerFromInterval(0, colors.length - 1)];
+}
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
